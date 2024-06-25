c27f62ce479cb2cad022bc6ebbb3d3012a106a63
Shouting-Crow <jinwook5062@gmail.com>
2024-06-26 08:12:32 +0900

    [2] Member 생성 로직과 테스트 완료

---
+++ src/main/java/com/clonecode/boardweb/domain/Member.java
@@ -0,0 +1,39 @@
+package com.clonecode.boardweb.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @OneToMany(mappedBy = "member")
+    private List<Board> boards = new ArrayList<>();
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String loginId;
+
+    @NotEmpty
+    private String password;
+
+    @Embedded
+    private Address address;
+
+    @NotEmpty
+    private String phoneNumber;
+
+    @NotEmpty
+    private String nickname;
+}
